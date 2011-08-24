3db6857797dabd8e4dc7a0b7f49ceb8d128ab5d3
Raphael Kimmig <raphael@whothefuck.(none)>
2011-08-24 20:38:03 +0200

    misc

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -120,7 +120,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django_extensions',
-    'south'
+    'south',
+    'core',
 )
 
 # A sample logging configuration. The only tangible logging
