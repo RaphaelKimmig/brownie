20c9c8fe092f6fca7ad502289cf65ad4087731ed
Raphael Kimmig <raphael@whothefuck.(none)>
2011-08-24 19:31:09 +0200

    3.1 done

---
+++ pathfinder/settings_local_sample.py
@@ -0,0 +1,7 @@
+MEDIA_ROOT = '/home/raphael/Source/work/pathfinder/media'
+
+TEMPLATE_DIRS = ('/home/raphael/Source/work/pathfinder/templates',)
+
+STATICFILES_DIRS = (
+    '/home/raphael/Source/work/pdm/static_media/',
+)
