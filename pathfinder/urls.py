60f0cc500df0377408f1a219468477f7b239bf3f
Raphael Kimmig <raphael@whothefuck.(none)>
2011-08-24 20:01:59 +0200

    misc

--- pathfinder/urls.py
+++ pathfinder/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,4 +16,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
