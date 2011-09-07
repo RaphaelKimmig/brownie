714049e6ff57324416973a00ada19527755d44da
Hannes Wächter <hwaechter@vorstieg.net>
2011-09-08 01:18:27 +0200

    added weapon types...

--- pathfinder/items/models.py
+++ pathfinder/items/models.py
@@ -7,10 +7,10 @@ class BaseItem(models.Model):
     _price = models.DecimalField(default=0, max_digits=32, decimal_places=2, verbose_name=_("Price in gold coins"))
     _weight = models.DecimalField(default=0, max_digits=32, decimal_places=1, verbose_name=_("Weight in pounds"))
 
-    _name = models.CharField(max_length=255, default='')
-    _description = models.TextField(blank=True, default='')
+    _name = models.CharField(max_length=255, default='', verbose_name=_("Name"))
+    _description = models.TextField(blank=True, default='', verbose_name=_("Description"))
 
-    _image = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True, null=True)
+    _image = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True, null=True, verbose_name=_("Image"))
 
     class Meta:
         abstract = True
@@ -29,21 +29,39 @@ WEAPON_TYPES = (
     ("bludgeoning", _("Bludgeoning")),
     ("piercing",_("Piercing")),
     ("slashing", _("Slashing")),
-    ("piercing_slashing", _("Piercing and slashing"))
+    ("piercing_or_slashing", _("Piercing or Slashing")),
+    ("bludgeoning_or_piercing", _("Bludgeoning or Piercing")),
+    ("bludgeoning_and_piercing", _("Bludgeoning and Piercing"))
     )
     
-WEAPON_CATEGORY = (
+WEAPON_USEFULNESS = (
     ("meele", _("Meele")),
     ("projectile", _("Projectile")),
     ("thrown", _("Thrown")),
     ("thrown_only", _("Thrown only"))
     )
+    
+WEAPON_ENCUMBRANCE = (
+    ("light", _("Light")),
+    ("one_handed", _("One-Handed")),
+    ("two_handed", _("Two-Handed"))
+    )
+    
+WEAPON_TRAINING = (
+    ("simple", _("Simple")),
+    ("martial", _("Martial")),
+    ("exotic", _("Exotic"))
+    )
 
 class Weapon(BaseItem):
-    _damage = models.CharField(max_length=20)
-    _critical = models.CharField(max_length=20)
-    _type = models.CharField(max_length=20, choices=WEAPON_TYPES)
-    
+    _damage = models.CharField(max_length=255, default="1d8", verbose_name=_("Demage"))
+    _critical = models.CharField(max_length=255, default="20/x2", verbose_name=_("Critical"))
+    _type = models.CharField(max_length=255, choices=WEAPON_TYPES, default="slashing", verbose_name=_("Type"))
+    _usefulness = models.CharField(max_length=255, choices=WEAPON_USEFULNESS, default="meele", verbose_name=_("Usefulness"))
+    _encumbrance = models.CharField(max_length=255, choices=WEAPON_ENCUMBRACE, default="one_handed", verbose_name=_("Encumbrance"))
+    _training = models.CharField(max_length=255, choices=WEAPON_TRAINING, default="martial", verbose_name=_("Training"))
+    _range = models.IntegerField(default=0, verbose_name=_("Range in feet"))
+    _ammunition = models.ManyToManyField(Ammunition)
     
     # weapon specials
     _special_brace = models.BooleanField(default=False)
@@ -54,7 +72,9 @@ class Weapon(BaseItem):
     _special_trip = models.BooleanField(default=False)
     _special_nonleathal = models.BooleanField(default=False)
 
-
 class Armor(BaseItem):
+    pass
+    
+class Ammunition(BaseItem):
     pass
     
