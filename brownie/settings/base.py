20376f312ea7d115b1539866abf65d48da57378c
Raphael Kimmig <raphael.kimmig@ampad.de>
2011-09-24 14:54:38 +0200

    new settings

---
+++ brownie/settings/base.py
@@ -0,0 +1,156 @@
+# Django settings for pdm project.
+from os import path
+
+settings_dir = path.dirname(__file__)
+PROJECT_ROOT = path.abspath(path.dirname(settings_dir))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': path.join(PROJECT_ROOT, 'base.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Berlin'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+MEDIA_BASE_DIR = path.realpath(path.join(PROJECT_ROOT, "media"))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = path.realpath(path.join(MEDIA_BASE_DIR, "dynamic"))
+
+STATIC_URL = '/media/static/'
+STATIC_ROOT = path.realpath(path.join(MEDIA_BASE_DIR, "static"))
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + '/admin/'
+STATICFILES_DIRS = (
+    path.realpath(path.join(PROJECT_ROOT, "static")),
+    )
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'm1e&75_7vtr(@7zkht4cjo!leu@hq4075hg9lcg$1cbzxww#--'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    # default template context processors
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+
+    # required by django-admin-tools
+    'django.core.context_processors.request',
+    )
+
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'brownie.urls'
+
+TEMPLATE_DIRS = (
+    path.join(PROJECT_ROOT, "templates"),
+    )
+
+PREREQ_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'grappelli',
+    'django.contrib.admin',
+    'django_extensions',
+    'south',
+    'taggit',
+    'taggit_autosuggest',
+    'django.contrib.admin',
+    )
+
+PROJECT_APPS = (
+    'core',
+    'items',
+    )
+
+INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
+
+TEST_RUNNER = 'testrunner.OurCoverageRunner'
+
+COVERAGE_MODULE_EXCLUDES = [
+    'tests$', 'settings$', 'urls$', 'locale$',
+    'migrations', 'fixtures', 'admin$',
+    ]
+COVERAGE_MODULE_EXCLUDES += PREREQ_APPS
+COVERAGE_REPORT_HTML_OUTPUT_DIR = path.join(PROJECT_ROOT, "coverage")
+
+HTML_OUTPUT_DIR = path.join(PROJECT_ROOT, "coverage")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+            },
+        },
+    }
+
+FILE_UPLOAD_PERMISSIONS = 0640
